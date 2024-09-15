e5ed41a5fca730436dac494989646910537a655b
lonewolf022 <87300897+lonewolf022@users.noreply.github.com>
2024-09-15 10:33:29 +0700

    Update terraform-env-vars-ctrl.groovy - Add parameter & Debug Stage

--- terraform-env-vars-ctrl.groovy
+++ terraform-env-vars-ctrl.groovy
@@ -1,12 +1,26 @@
 pipeline {
     agent any
 
+    parameters {
+        choice(name: 'TF_VERSION', choices: ['1.5.7', '1.9.5'], description: 'Select the terraform version')
+        choice(name: 'AZ_PV_VERSION', choices: ['3.0', '4.0'], description: 'Select the Azure provider version')
+    }
+    
     environment {
-        TF_VERSION = '1.5.7' // Specify the desired Terraform version
-        PROVIDER_VERSION = '~> 3.0' // Specify the desired azurerm provider version
+        TF_VERSION = '${TF_VERSION}' // Specify the desired Terraform version
+        PROVIDER_VERSION = '~> ${AZ_PV_VERSION}' // Specify the desired azurerm provider version
     }
 
     stages {
+        stage('Debug') {
+            steps {
+                script {
+                    echo "Version for Terraform: ${params.TF_VERSION}"
+                    echo "Version for Azure Provider: ${params.AZ_PV_VERSION}"
+                }
+            }
+        }
+        
         stage('Install Terraform') {
             steps {
                 sh '''
